2a3d5c00b744491e7caebab01febb6c234762c44
v1sanjay <vaibhav.sanjay@outlook.com>
2019-01-08 19:46:49 -0500

    Movement
    It moves backwards, forwards, left, and right.

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.Translate(new Vector3(Input.GetAxis("Horizontal") * 0.25f, 0, Input.GetAxis("Vertical") * 0.25f));
+    }
+}
