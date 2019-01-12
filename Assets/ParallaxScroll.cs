bb1eb8896f27190a86aa278a7365b51f064c5897
Atlinux <atlinuxtm@gmail.com>
2019-01-12 16:24:48 -0500

    Parallax scrolling added

---
+++ Assets/ParallaxScroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScroll : MonoBehaviour
+{
+    public Transform target;
+    public Vector2 speed;
+
+    public void Update()
+    {
+        transform.position = target.position * speed;
+    }
+}
